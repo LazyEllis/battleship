98730f75495c5c666501b276cde7936ca62037d3
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2024-05-21 01:00:56 +0100

    feat: Add manual player ship placement logic
    - Updated index.html to include new elements:
      - Added a rotate button for changing ship placement direction.
      - Added a game message element for instructions.
    
    - Updates style.scss for updated styles:
      - Defined new color variables and mixins for consistent styling.
      - Styled grids, buttons, modals, and game elements.
      - Added responsive design for better user experience on different
      screen sizes.
    
    - Updated dom.js for enhanced DOM interactions:
      - Functions for clearing grids, setting ship messages, and rendering
        grids.
      - Functions for toggling grid states, showing game messages, and
        highlighting ship previews.
      - Functions for displaying and hiding game over modal.
    
    - Updated index.js to implement game logic:
      - Handled player and computer ship placement with direction changes.
      - Added ship preview functionality for better placement visualization.
      - Managed game state transitions, user interactions, and game restart
        logic.

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,9 +1,49 @@
-import { clearGrid, setCellType } from "./utils";
-
+const gameMessage = document.querySelector(".game-message");
+const rotateBtn = document.querySelector(".rotate-btn");
 const playerGrid = document.querySelector(".player-grid");
 const computerGrid = document.querySelector(".computer-grid");
+const grids = document.querySelectorAll(".grid");
 const gameOverModal = document.querySelector(".game-over-modal");
 const gameOverMessage = document.querySelector(".game-over-message");
+
+const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+
+const clearGrid = (grid) => {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+};
+
+const setShipMessage = (length) => {
+  switch (length) {
+    case 4:
+      return "Place your Battleship";
+    case 3:
+      return "Place your Cruisers";
+    case 2:
+      return "Place your Destroyers";
+    case 1:
+      return "Place your Submarines";
+    default:
+      return "";
+  }
+};
+
+const setCellType = (player, cell, value) => {
+  if (player.name === "Player" && typeof value === "object") {
+    cell.classList.add("ship");
+  } else if (value === "miss") {
+    cell.classList.add("miss");
+  } else if (value === "hit") {
+    cell.classList.add("hit");
+  }
+};
+
+export const toggleGrids = () => {
+  grids.forEach((grid) => {
+    grid.classList.toggle("enabled");
+  });
+};
 
 export const renderGrid = (player) => {
   const grid = player.name === "Player" ? playerGrid : computerGrid;
@@ -21,9 +61,69 @@ export const renderGrid = (player) => {
   });
 };
 
+const showGameMessage = () => {
+  const { index } = gameMessage.dataset;
+  gameMessage.dataset.length = shipLengths[+index];
+  const { length } = gameMessage.dataset;
+  const message = setShipMessage(+length);
+  gameMessage.textContent = message;
+};
+
+export const resetGameMessage = () => {
+  gameMessage.dataset.index = 0;
+  showGameMessage();
+};
+
+export const changeGameMessage = () => {
+  const { index } = gameMessage.dataset;
+  gameMessage.dataset.index = +index + 1;
+  showGameMessage();
+};
+
+export const changeDirection = () => {
+  const { direction } = rotateBtn.dataset;
+  rotateBtn.dataset.direction =
+    direction === "horizontal" ? "vertical" : "horizontal";
+};
+
 export const showGameOverModal = (message) => {
   gameOverMessage.textContent = message;
   gameOverModal.showModal();
+};
+
+export const highlightShip = (row, column, length, direction) => {
+  const cellsToHighlight = [];
+  let isValid = true;
+
+  for (let i = 0; i < length; i += 1) {
+    let cell;
+
+    if (direction === "horizontal") {
+      cell = playerGrid.querySelector(
+        `[data-row="${+row}"][data-column="${+column + i}"]`
+      );
+    } else {
+      cell = playerGrid.querySelector(
+        `[data-row="${+row + i}"][data-column="${+column}"]`
+      );
+    }
+
+    if (!cell || cell.classList.contains("ship")) {
+      isValid = false;
+      break;
+    }
+
+    cellsToHighlight.push(cell);
+  }
+
+  if (isValid) {
+    cellsToHighlight.forEach((cell) => cell.classList.add("preview"));
+  }
+};
+
+export const removePreview = () => {
+  const previews = document.querySelectorAll(".preview");
+  previews.forEach((preview) => preview.classList.remove("preview"));
 };
 
 export const hideGameOverModal = () => {
--- src/js/index.js
+++ src/js/index.js
@@ -2,46 +2,49 @@ import "../style.scss";
 import Ship from "./ship";
 import Gameboard from "./gameboard";
 import Player from "./player";
-import { renderGrid, showGameOverModal, hideGameOverModal } from "./dom";
 import {
-  generateRandomCoordinate,
-  generateRandomOrientation,
-  generateRandomValidCoordinate,
-  isCellMissed,
-} from "./utils";
+  renderGrid,
+  changeDirection,
+  changeGameMessage,
+  highlightShip,
+  resetGameMessage,
+  removePreview,
+  showGameOverModal,
+  hideGameOverModal,
+  toggleGrids,
+} from "./dom";
 
+const gameMessage = document.querySelector(".game-message");
 const computerGrid = document.querySelector(".computer-grid");
+const playerGrid = document.querySelector(".player-grid");
+const rotateBtn = document.querySelector(".rotate-btn");
 const restartBtn = document.querySelector(".restart-btn");
 
 const humanPlayer = new Player("Player", new Gameboard());
 const computerPlayer = new Player("Computer", new Gameboard());
 
-const placeRandomShip = (ship, player) => {
-  let placed = false;
-
-  while (!placed) {
-    try {
-      const coordinate = generateRandomCoordinate();
-      const orientation = generateRandomOrientation();
-      player.gameboard.placeShip(ship, coordinate, orientation);
-      placed = true;
-    } catch (error) {
-      // Error caught. This block intentionally left empty to retry placing the ship.
-    }
-  }
+const generateRandomCoordinate = () => {
+  const row = Math.floor(Math.random() * 10);
+  const column = Math.floor(Math.random() * 10);
+  return [row, column];
 };
 
-const placeShipsOverLengths = (player, shipLengths) => {
-  shipLengths.forEach((length) => {
-    const ship = new Ship(length);
-    placeRandomShip(ship, player);
-  });
-};
+const generateRandomOrientation = () =>
+  Math.random() < 0.5 ? "horizontal" : "vertical";
 
-const placeRandomShips = () => {
-  const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
-  placeShipsOverLengths(computerPlayer, shipLengths);
-  placeShipsOverLengths(humanPlayer, shipLengths);
+const isCellHit = (player, coordinate) =>
+  player.gameboard.grid[coordinate[0]][coordinate[1]] === "hit";
+
+const isCellMissed = (player, coordinate) =>
+  player.gameboard.grid[coordinate[0]][coordinate[1]] === "miss";
+
+const generateRandomValidCoordinate = (player) => {
+  let coordinate = generateRandomCoordinate();
+
+  while (isCellHit(player, coordinate) || isCellMissed(player, coordinate))
+    coordinate = generateRandomCoordinate();
+
+  return coordinate;
 };
 
 const renderGrids = () => {
@@ -49,12 +52,73 @@ const renderGrids = () => {
   renderGrid(computerPlayer);
 };
 
-const restartGame = () => {
-  humanPlayer.gameboard = new Gameboard();
-  computerPlayer.gameboard = new Gameboard();
-  hideGameOverModal();
-  placeRandomShips();
+const placeRandomShip = (ship, player) => {
+  let placed = false;
+
+  while (!placed) {
+    try {
+      const coordinate = generateRandomCoordinate();
+      const direction = generateRandomOrientation();
+      player.gameboard.placeShip(ship, coordinate, direction);
+      placed = true;
+    } catch (error) {
+      // Error caught. This block intentionally left empty to retry placing the ship.
+    }
+  }
+};
+
+const placeComputerShips = () => {
+  const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+  shipLengths.forEach((length) => {
+    const ship = new Ship(length);
+    placeRandomShip(ship, computerPlayer);
+  });
+};
+
+const startGame = () => {
   renderGrids();
+  toggleGrids();
+  placeComputerShips();
+};
+
+const previewShip = (e) => {
+  if (
+    e.target.classList.contains("ship") ||
+    !e.target.parentNode.classList.contains("enabled")
+  )
+    return;
+
+  if (e.target.classList.contains("cell")) {
+    const { row, column } = e.target.dataset;
+    const { length } = gameMessage.dataset;
+    const { direction } = rotateBtn.dataset;
+    highlightShip(+row, +column, +length, direction);
+  }
+};
+
+const placePlayerShip = (e) => {
+  if (
+    e.target.classList.contains("ship") ||
+    !e.target.parentNode.classList.contains("enabled")
+  )
+    return;
+
+  if (e.target.classList.contains("cell")) {
+    const { length } = gameMessage.dataset;
+    const ship = new Ship(+length);
+    const coordinate = [+e.target.dataset.row, +e.target.dataset.column];
+    const { direction } = rotateBtn.dataset;
+
+    try {
+      humanPlayer.gameboard.placeShip(ship, coordinate, direction);
+    } catch {
+      return;
+    }
+
+    renderGrids();
+    changeGameMessage();
+    if (gameMessage.textContent === "") startGame();
+  }
 };
 
 const changeGameStatus = (player, coordinate) => {
@@ -65,11 +129,17 @@ const changeGameStatus = (player, coordinate) => {
 const isGameOver = () => {
   if (humanPlayer.gameboard.isAllSunk()) {
     showGameOverModal("You Lose!");
-    return true;
   }
 
   if (computerPlayer.gameboard.isAllSunk()) {
     showGameOverModal("You Win!");
+  }
+
+  if (
+    humanPlayer.gameboard.isAllSunk() ||
+    computerPlayer.gameboard.isAllSunk()
+  ) {
+    toggleGrids();
     return true;
   }
 
@@ -88,15 +158,14 @@ const hitPlayerCell = () => {
 };
 
 const hitComputerCell = (e) => {
-  if (computerPlayer.gameboard.isAllSunk()) return;
+  if (!e.target.parentNode.classList.contains("enabled")) return;
 
   if (
     e.target.classList.contains("cell") &&
     !e.target.classList.contains("miss") &&
     !e.target.classList.contains("hit")
   ) {
-    const cell = e.target;
-    const { row, column } = cell.dataset;
+    const { row, column } = e.target.dataset;
     const coordinate = [row, column];
     changeGameStatus(computerPlayer, coordinate);
     if (isGameOver()) return;
@@ -104,8 +173,20 @@ const hitComputerCell = (e) => {
   }
 };
 
+const restartGame = () => {
+  resetGameMessage();
+  humanPlayer.gameboard = new Gameboard();
+  computerPlayer.gameboard = new Gameboard();
+  hideGameOverModal();
+  renderGrids();
+};
+
+rotateBtn.addEventListener("click", changeDirection);
+playerGrid.addEventListener("click", placePlayerShip);
+playerGrid.addEventListener("mouseover", previewShip);
+playerGrid.addEventListener("mouseout", removePreview);
 computerGrid.addEventListener("click", hitComputerCell);
 restartBtn.addEventListener("click", restartGame);
 
-placeRandomShips();
 renderGrids();
+resetGameMessage();
